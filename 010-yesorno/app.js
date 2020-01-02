ca8c3ab3ba3db4ea10c20362269f0f2d6a1780cb
branthnl <branthnl1@gmail.com>
2020-01-02 08:05:36 -0700

    added char selection

--- 010-yesorno/app.js
+++ 010-yesorno/app.js
@@ -1,4 +1,72 @@
 const yn = document.querySelector('.yn');
+const ynMenu = document.querySelector('.ynMenu');
+const ynMenuImg = document.querySelector('.ynMenu > div > img');
+const ynMenuH3 = document.querySelector('.ynMenu > div > h3');
+const ynShop = document.querySelector('.ynShop');
+
+const charList = [
+	'bear',
+	'buffalo',
+	'chick',
+	'chicken',
+	'cow',
+	'crocodile',
+	'dog',
+	'duck',
+	'elephant',
+	'frog',
+	'giraffe',
+	'goat',
+	'gorilla',
+	'hippo',
+	'horse',
+	'monkey',
+	'moose',
+	'narwhal',
+	'owl',
+	'panda',
+	'parrot',
+	'penguin',
+	'pig',
+	'rabbit',
+	'rhino',
+	'sloth',
+	'snake',
+	'walrus',
+	'whale',
+	'zebra'
+];
+
+const capitalize = (s) => s.toLowerCase().split('').map((v, i) => (i === 0? v.toUpperCase() : v)).join('');
+
+if (sessionStorage.getItem('char') != null) {
+	const char = sessionStorage.getItem('char');
+	ynMenuImg.setAttribute('src', 'images/chars/' + char + '.png');
+	ynMenuH3.innerHTML = capitalize(char);
+}
+
+function changeChar(char) {
+	ynMenuImg.setAttribute('src', 'images/chars/' + char + '.png');
+	ynMenuH3.innerHTML = capitalize(char);
+	sessionStorage.setItem('char', char);
+	const charImgs = ynShop.querySelectorAll('img');
+	for (let i = 0; i < charImgs.length; i++) {
+		charImgs[i].classList.remove('selected');
+		if (charImgs[i].getAttribute('src').includes(char)) {
+			charImgs[i].classList.add('selected');
+		}
+	}
+}
+
+for (char of charList) {
+	const img = document.createElement('img');
+	img.setAttribute('src', 'images/chars/' + char + '.png');
+	if (char === 'bear') {
+		img.classList.add('selected');
+	}
+	img.setAttribute('onclick', `changeChar("${char}")`);
+	ynShop.appendChild(img);
+}
 
 const update = window.requestAnimationFrame
 	|| window.msRequestAnimationFrame

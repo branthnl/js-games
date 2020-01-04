c8886f120b239a99724551d90218bca0f22295f1
branthnl <branthnl1@gmail.com>
2020-01-04 03:01:29 -0700

    froggy concept

--- 013-froggy/app.js
+++ 013-froggy/app.js
@@ -1,40 +1,138 @@
-class Frog extends BranthObject {
-	start() {
-		//
-	}
-	update() {
-		//
-	}
+class Background extends BranthObject {
 	render() {
-		Draw.setColor(C.White);
-		Draw.ellipse(this.x, this.y, 50, 27);
-		Draw.polyBegin(Poly.fill);
-		Draw.vertex(400, 400);
-		Draw.vertex(420, 420);
-		Draw.vertex(400, 440);
-		Draw.vertex(380, 420);
-		Draw.polyEnd();
-		Draw.polyBegin(Poly.stroke);
-		Draw.vertex(300, 300);
-		Draw.vertex(320, 320);
-		Draw.vertex(300, 340);
-		Draw.vertex(280, 320);
-		Draw.polyEnd();
+		Draw.setColor(C.lightSkyBlue);
+		Draw.rect(0, 0, Room.w, Room.h);
 	}
 }
 
+class LandOrigin extends BranthObject {
+	render() {
+		Draw.setColor(C.darkOrange);
+		Draw.roundrect(0, -20, Room.w, 70, 20);
+		Draw.setColor(C.brown);
+		Draw.roundrect(0, -20, Room.w, 70, 20, true);
+	}
+}
+
+class LandEnd extends BranthObject {
+	render() {
+		Draw.setColor(C.limeGreen);
+		Draw.roundrect(0, Room.h - 50, Room.w, 70, 20);
+		Draw.setColor(C.seaGreen);
+		Draw.roundrect(0, Room.h - 50, Room.w, 70, 20, true);
+	}
+}
+
+class Lotus extends BranthObject {
+	start() {
+		this.r = Math.range(28, 32);
+		this.d = Math.range(0, 360);
+		this.spd = Math.range(2, 2.5);
+		this.dinc = Math.range(-0.5, 0.5);
+		this.move = Math.randneg();
+		this.off = {
+			x: Math.range(-3, 3),
+			y: Math.range(-3, 3)
+		}
+	}
+	update() {
+		if (this.x + this.r >= Room.w) {
+			this.move = -1;
+		}
+		if (this.x - this.r <= 0) {
+			this.move = 1;
+		}
+		this.x += this.spd * this.move * Time.scaledDeltaTime;
+		this.d += this.dinc * Time.scaledDeltaTime;
+	}
+	render() {
+		const off = {
+			x: this.x + this.off.x,
+			y: this.y + this.off.y
+		}
+		Draw.polyBegin();
+		Draw.vertex(off.x + Math.lendirx(this.r * 0.5, this.d),
+					off.y + Math.lendiry(this.r * 0.5, this.d));
+		Draw.vertex(off.x + Math.lendirx(this.r, this.d + 15),
+					off.y + Math.lendiry(this.r, this.d + 15));
+		Draw.vertex(off.x + Math.lendirx(this.r * 1.3, this.d + 70),
+					off.y + Math.lendiry(this.r * 1.3, this.d + 70));
+		Draw.vertex(off.x + Math.lendirx(this.r * 1.4, this.d + 140),
+					off.y + Math.lendiry(this.r * 1.4, this.d + 140));
+		Draw.vertex(off.x + Math.lendirx(this.r, this.d + 187),
+					off.y + Math.lendiry(this.r, this.d + 187));
+		Draw.vertex(off.x + Math.lendirx(this.r * 1.3, this.d - 125),
+					off.y + Math.lendiry(this.r * 1.3, this.d - 125));
+		Draw.vertex(off.x + Math.lendirx(this.r * 1.3, this.d - 70),
+					off.y + Math.lendiry(this.r * 1.3, this.d - 70));
+		Draw.vertex(off.x + Math.lendirx(this.r, this.d - 15),
+					off.y + Math.lendiry(this.r, this.d - 15));
+		Draw.beginPath();
+		Draw.moveTo(Draw.vertices[0].x, Draw.vertices[0].y);
+		Draw.lineTo(Draw.vertices[1].x, Draw.vertices[1].y);
+		Draw.bezierCurveTo(
+			Draw.vertices[2].x, Draw.vertices[2].y,
+			Draw.vertices[3].x, Draw.vertices[3].y,
+			Draw.vertices[4].x, Draw.vertices[4].y
+		);
+		Draw.bezierCurveTo(
+			Draw.vertices[5].x, Draw.vertices[5].y,
+			Draw.vertices[6].x, Draw.vertices[6].y,
+			Draw.vertices[7].x, Draw.vertices[7].y
+		);
+		Draw.closePath();
+		Draw.setColor(C.limeGreen);
+		Draw.draw();
+		Draw.setLineWidth(3);
+		Draw.setColor(C.seaGreen);
+		Draw.draw(true);
+		Draw.resetLineWidth();
+	}
+}
+
+class Frog extends BranthObject {
+	start() {
+		this.jumpRange = 75;
+		this.alarm[0] = 1000;
+	}
+	update() {
+		this.alarmUpdate();
+	}
+	render() {
+		Draw.setColor(C.yellow);
+		Draw.star(this.x, this.y, 10);
+		Draw.setColor(C.black);
+		Draw.star(this.x, this.y, 10, true);
+	}
+	alarm0() {
+		this.y -= this.jumpRange;
+		if (this.y - this.jumpRange > 0) this.alarm[0] = 1000;
+	}
+}
+
+OBJ.add(Background);
+OBJ.add(LandOrigin);
+OBJ.add(LandEnd);
+OBJ.add(Lotus);
 OBJ.add(Frog);
-const Menu = new BranthRoom('Menu', 640, 640);
-const Game = new BranthRoom('Game', 240, 360);
+
+const Menu = new BranthRoom('Menu', 360, 640);
+const Game = new BranthRoom('Game', 360, 640);
 Room.add(Menu);
 Room.add(Game);
 
 Menu.start = () => {
-	OBJ.create(Frog, 45, 45);
+	Room.start('Game');
 }
 
 Game.start = () => {
-	console.log('game');
+	OBJ.create(Background);
+	OBJ.create(LandOrigin);
+	OBJ.create(LandEnd);
+	for (let i = 0; i < 7; i++) {
+		OBJ.create(Lotus, Math.range(0, Room.w), Room.h - 97 - 75 * i);
+	}
+	OBJ.create(Frog, Room.w.mid(), Room.h - 22);
 }
 
 BRANTH.start();
--- 013-froggy/branth.js
+++ 013-froggy/branth.js
@@ -13,12 +13,17 @@ const CANVAS = document.createElement('canvas');
 const CTX = CANVAS.getContext('2d');
 const FRAME_RATE = 1000 / 60;
 const DEFAULT_FONT = 'sans-serif';
+const DEBUG_MODE = true;
+if (!DEBUG_MODE) console.log = () => {};
 
 const Time = {
 	time: 0,
 	lastTime: 0,
 	deltaTime: 0,
 	fixedDeltaTime: FRAME_RATE,
+	get scaledDeltaTime() {
+		return this.deltaTime / this.fixedDeltaTime;
+	},
 	update(t) {
 		this.lastTime = this.time || 0;
 		this.time = t || 0;
@@ -27,154 +32,154 @@ const Time = {
 };
 
 const C = {
-	AliceBlue: '#F0F8FF',
-	AntiqueWhite: '#FAEBD7',
-	Aqua: '#00FFFF',
-	Aquamarine: '#7FFFD4',
-	Azure: '#F0FFFF',
-	Beige: '#F5F5DC',
-	Bisque: '#FFE4C4',
-	Black: '#000000',
-	BlanchedAlmond: '#FFEBCD',
-	Blue: '#0000FF',
-	BlueViolet: '#8A2BE2',
-	Brown: '#A52A2A',
-	BurlyWood: '#DEB887',
-	CadetBlue: '#5F9EA0',
-	Chartreuse: '#7FFF00',
-	Chocolate: '#D2691E',
-	Coral: '#FF7F50',
-	CornflowerBlue: '#6495ED',
-	Cornsilk: '#FFF8DC',
-	Crimson: '#DC143C',
-	Cyan: '#00FFFF',
-	DarkBlue: '#00008B',
-	DarkCyan: '#008B8B',
-	DarkGoldenRod: '#B8860B',
-	DarkGray: '#A9A9A9',
-	DarkGrey: '#A9A9A9',
-	DarkGreen: '#006400',
-	DarkKhaki: '#BDB76B',
-	DarkMagenta: '#8B008B',
-	DarkOliveGreen: '#556B2F',
-	DarkOrange: '#FF8C00',
-	DarkOrchid: '#9932CC',
-	DarkRed: '#8B0000',
-	DarkSalmon: '#E9967A',
-	DarkSeaGreen: '#8FBC8F',
-	DarkSlateBlue: '#483D8B',
-	DarkSlateGray: '#2F4F4F',
-	DarkSlateGrey: '#2F4F4F',
-	DarkTurquoise: '#00CED1',
-	DarkViolet: '#9400D3',
-	DeepPink: '#FF1493',
-	DeepSkyBlue: '#00BFFF',
-	DimGray: '#696969',
-	DimGrey: '#696969',
-	DodgerBlue: '#1E90FF',
-	FireBrick: '#B22222',
-	FloralWhite: '#FFFAF0',
-	ForestGreen: '#228B22',
-	Fuchsia: '#FF00FF',
-	Gainsboro: '#DCDCDC',
-	GhostWhite: '#F8F8FF',
-	Gold: '#FFD700',
-	GoldenRod: '#DAA520',
-	Gray: '#808080',
-	Grey: '#808080',
-	Green: '#008000',
-	GreenYellow: '#ADFF2F',
-	HoneyDew: '#F0FFF0',
-	HotPink: '#FF69B4',
-	IndianRed: '#CD5C5C',
-	Indigo: '#4B0082',
-	Ivory: '#FFFFF0',
-	Khaki: '#F0E68C',
-	Lavender: '#E6E6FA',
-	LavenderBlush: '#FFF0F5',
-	LawnGreen: '#7CFC00',
-	LemonChiffon: '#FFFACD',
-	LightBlue: '#ADD8E6',
-	LightCoral: '#F08080',
-	LightCyan: '#E0FFFF',
-	LightGoldenRodYellow: '#FAFAD2',
-	LightGray: '#D3D3D3',
-	LightGrey: '#D3D3D3',
-	LightGreen: '#90EE90',
-	LightPink: '#FFB6C1',
-	LightSalmon: '#FFA07A',
-	LightSeaGreen: '#20B2AA',
-	LightSkyBlue: '#87CEFA',
-	LightSlateGray: '#778899',
-	LightSlateGrey: '#778899',
-	LightSteelBlue: '#B0C4DE',
-	LightYellow: '#FFFFE0',
-	Lime: '#00FF00',
-	LimeGreen: '#32CD32',
-	Linen: '#FAF0E6',
-	Magenta: '#FF00FF',
-	Maroon: '#800000',
-	MediumAquaMarine: '#66CDAA',
-	MediumBlue: '#0000CD',
-	MediumOrchid: '#BA55D3',
-	MediumPurple: '#9370DB',
-	MediumSeaGreen: '#3CB371',
-	MediumSlateBlue: '#7B68EE',
-	MediumSpringGreen: '#00FA9A',
-	MediumTurquoise: '#48D1CC',
-	MediumVioletRed: '#C71585',
-	MidnightBlue: '#191970',
-	MintCream: '#F5FFFA',
-	MistyRose: '#FFE4E1',
-	Moccasin: '#FFE4B5',
-	NavajoWhite: '#FFDEAD',
-	Navy: '#000080',
-	OldLace: '#FDF5E6',
-	Olive: '#808000',
-	OliveDrab: '#6B8E23',
-	Orange: '#FFA500',
-	OrangeRed: '#FF4500',
-	Orchid: '#DA70D6',
-	PaleGoldenRod: '#EEE8AA',
-	PaleGreen: '#98FB98',
-	PaleTurquoise: '#AFEEEE',
-	PaleVioletRed: '#DB7093',
-	PapayaWhip: '#FFEFD5',
-	PeachPuff: '#FFDAB9',
-	Peru: '#CD853F',
-	Pink: '#FFC0CB',
-	Plum: '#DDA0DD',
-	PowderBlue: '#B0E0E6',
-	Purple: '#800080',
-	RebeccaPurple: '#663399',
-	Red: '#FF0000',
-	RosyBrown: '#BC8F8F',
-	RoyalBlue: '#4169E1',
-	SaddleBrown: '#8B4513',
-	Salmon: '#FA8072',
-	SandyBrown: '#F4A460',
-	SeaGreen: '#2E8B57',
-	SeaShell: '#FFF5EE',
-	Sienna: '#A0522D',
-	Silver: '#C0C0C0',
-	SkyBlue: '#87CEEB',
-	SlateBlue: '#6A5ACD',
-	SlateGray: '#708090',
-	SlateGrey: '#708090',
-	Snow: '#FFFAFA',
-	SpringGreen: '#00FF7F',
-	SteelBlue: '#4682B4',
-	Tan: '#D2B48C',
-	Teal: '#008080',
-	Thistle: '#D8BFD8',
-	Tomato: '#FF6347',
-	Turquoise: '#40E0D0',
-	Violet: '#EE82EE',
-	Wheat: '#F5DEB3',
-	White: '#FFFFFF',
-	WhiteSmoke: '#F5F5F5',
-	Yellow: '#FFFF00',
-	YellowGreen: '#9ACD32'
+	aliceBlue: '#f0f8ff',
+	antiqueWhite: '#faebd7',
+	aqua: '#00ffff',
+	aquamarine: '#7fffd4',
+	azure: '#f0ffff',
+	beige: '#f5f5dc',
+	bisque: '#ffe4c4',
+	black: '#000000',
+	blanchedAlmond: '#ffebcd',
+	blue: '#0000ff',
+	blueViolet: '#8a2be2',
+	brown: '#a52a2a',
+	burlyWood: '#deb887',
+	cadetBlue: '#5f9ea0',
+	chartreuse: '#7fff00',
+	chocolate: '#d2691e',
+	coral: '#ff7f50',
+	cornflowerBlue: '#6495ed',
+	cornsilk: '#fff8dc',
+	crimson: '#dc143c',
+	cyan: '#00ffff',
+	darkBlue: '#00008b',
+	darkCyan: '#008b8b',
+	darkGoldenRod: '#b8860b',
+	darkGray: '#a9a9a9',
+	darkGrey: '#a9a9a9',
+	darkGreen: '#006400',
+	darkKhaki: '#bdb76b',
+	darkMagenta: '#8b008b',
+	darkOliveGreen: '#556b2f',
+	darkOrange: '#ff8c00',
+	darkOrchid: '#9932cc',
+	darkRed: '#8b0000',
+	darkSalmon: '#e9967a',
+	darkSeaGreen: '#8fbc8f',
+	darkSlateBlue: '#483d8b',
+	darkSlateGray: '#2f4f4f',
+	darkSlateGrey: '#2f4f4f',
+	darkTurquoise: '#00ced1',
+	darkViolet: '#9400d3',
+	deepPink: '#ff1493',
+	deepSkyBlue: '#00bfff',
+	dimGray: '#696969',
+	dimGrey: '#696969',
+	dodgerBlue: '#1e90ff',
+	fireBrick: '#b22222',
+	floralWhite: '#fffaf0',
+	forestGreen: '#228b22',
+	fuchsia: '#ff00ff',
+	gainsboro: '#dcdcdc',
+	ghostWhite: '#f8f8ff',
+	gold: '#ffd700',
+	goldenRod: '#daa520',
+	gray: '#808080',
+	grey: '#808080',
+	green: '#008000',
+	greenYellow: '#adff2f',
+	honeyDew: '#f0fff0',
+	hotPink: '#ff69b4',
+	indianRed: '#cd5c5c',
+	indigo: '#4b0082',
+	ivory: '#fffff0',
+	khaki: '#f0e68c',
+	lavender: '#e6e6fa',
+	lavenderBlush: '#fff0f5',
+	lawnGreen: '#7cfc00',
+	lemonChiffon: '#fffacd',
+	lightBlue: '#add8e6',
+	lightCoral: '#f08080',
+	lightCyan: '#e0ffff',
+	lightGoldenRodYellow: '#fafad2',
+	lightGray: '#d3d3d3',
+	lightGrey: '#d3d3d3',
+	lightGreen: '#90ee90',
+	lightPink: '#ffb6c1',
+	lightSalmon: '#ffa07a',
+	lightSeaGreen: '#20b2aa',
+	lightSkyBlue: '#87cefa',
+	lightSlateGray: '#778899',
+	lightSlateGrey: '#778899',
+	lightSteelBlue: '#b0c4de',
+	lightYellow: '#ffffe0',
+	lime: '#00ff00',
+	limeGreen: '#32cd32',
+	linen: '#faf0e6',
+	magenta: '#ff00ff',
+	maroon: '#800000',
+	mediumAquaMarine: '#66cdaa',
+	mediumBlue: '#0000cd',
+	mediumOrchid: '#ba55d3',
+	mediumPurple: '#9370db',
+	mediumSeaGreen: '#3cb371',
+	mediumSlateBlue: '#7b68ee',
+	mediumSpringGreen: '#00fa9a',
+	mediumTurquoise: '#48d1cc',
+	mediumVioletRed: '#c71585',
+	midnightBlue: '#191970',
+	mintCream: '#f5fffa',
+	mistyRose: '#ffe4e1',
+	moccasin: '#ffe4b5',
+	navajoWhite: '#ffdead',
+	navy: '#000080',
+	oldLace: '#fdf5e6',
+	olive: '#808000',
+	oliveDrab: '#6b8e23',
+	orange: '#ffa500',
+	orangeRed: '#ff4500',
+	orchid: '#da70d6',
+	paleGoldenRod: '#eee8aa',
+	paleGreen: '#98fb98',
+	paleTurquoise: '#afeeee',
+	paleVioletRed: '#db7093',
+	papayaWhip: '#ffefd5',
+	peachPuff: '#ffdab9',
+	peru: '#cd853f',
+	pink: '#ffc0cb',
+	plum: '#dda0dd',
+	powderBlue: '#b0e0e6',
+	purple: '#800080',
+	rebeccaPurple: '#663399',
+	red: '#ff0000',
+	rosyBrown: '#bc8f8f',
+	royalBlue: '#4169e1',
+	saddleBrown: '#8b4513',
+	salmon: '#fa8072',
+	sandyBrown: '#f4a460',
+	seaGreen: '#2e8b57',
+	seaShell: '#fff5ee',
+	sienna: '#a0522d',
+	silver: '#c0c0c0',
+	skyBlue: '#87ceeb',
+	slateBlue: '#6a5acd',
+	slateGray: '#708090',
+	slateGrey: '#708090',
+	snow: '#fffafa',
+	springGreen: '#00ff7f',
+	steelBlue: '#4682b4',
+	tan: '#d2b48c',
+	teal: '#008080',
+	thistle: '#d8bfd8',
+	tomato: '#ff6347',
+	turquoise: '#40e0d0',
+	violet: '#ee82ee',
+	wheat: '#f5deb3',
+	white: '#ffffff',
+	whiteSmoke: '#f5f5f5',
+	yellow: '#ffff00',
+	yellowGreen: '#9acd32'
 };
 
 const Font = {
@@ -204,59 +209,66 @@ const Cap = {
 };
 
 const Poly = {
-	fill: 'name: fill, closePath: true, outline: false',
-	stroke: 'name: stroke, closePath: true, outline: true',
-	lineList: 'name: lineList, closePath: false, outline: true',
-	pointList: 'name: pointList, closePath: false, outline: true',
-	triangleList: 'name: triangleList, closePath: false, outline: true',
-	triangleListFill: 'name: triangleList, closePath: false, outline: false'
+	fill: 'name: fill, quantity: 0, closePath: true, outline: false',
+	stroke: 'name: stroke, quantity: 0, closePath: true, outline: true',
+	lineList: 'name: lineList, quantity: 2, closePath: false, outline: true',
+	pointList: 'name: pointList, quantity: 1, closePath: false, outline: true',
+	triangleList: 'name: triangleList, quantity: 3, closePath: true, outline: true',
+	triangleListFill: 'name: triangleList, quantity: 3, closePath: false, outline: false'
 };
 
-class Draw {
-	static setAlpha(a) {
+const Draw = {
+	setAlpha(a) {
 		CTX.globalAlpha = a;
-	}
-	static setColor(color) {
+	},
+	setColor(color) {
 		CTX.fillStyle = color;
 		CTX.strokeStyle = color;
-	}
-	static setHAlign(align) {
+	},
+	setHAlign(align) {
 		CTX.textAlign = align;
-	}
-	static setVAlign(align) {
+	},
+	setVAlign(align) {
 		CTX.textBaseline = align;
-	}
-	static setHVAlign(h, v) {
+	},
+	setHVAlign(h, v) {
 		CTX.textAlign = h;
 		CTX.textBaseline = v || h;
-	}
-	static setFont(font) {
+	},
+	setFont(font) {
 		CTX.font = `${font} ${DEFAULT_FONT}`;
-	}
-	static text(x, y, text) {
+	},
+	text(x, y, text) {
 		CTX.fillText(text, x, y);
-	}
-	static draw(outline, cap) {
+	},
+	draw(outline, cap) {
 		if (outline) {
 			if (cap) CTX.lineCap = cap;
 			CTX.stroke();
+			this.resetCap();
 		}
 		else {
 			CTX.fill();
 		}
-	}
-	static rect(x, y, w, h, outline) {
+	},
+	line(x1, y1, x2, y2, cap) {
+		CTX.beginPath();
+		CTX.moveTo(x1, y1);
+		CTX.lineTo(x2, y2);
+		this.draw(true, cap);
+	},
+	rect(x, y, w, h, outline) {
 		CTX.beginPath();
 		CTX.rect(x, y, w, h);
 		this.draw(outline);
-	}
-	static circle(x, y, r, outline) {
+	},
+	circle(x, y, r, outline) {
 		CTX.beginPath();
 		CTX.arc(x, y, r, 0, 2 * Math.PI);
 		CTX.closePath();
 		this.draw(outline);
-	}
-	static ellipse(x, y, w, h, outline) {
+	},
+	ellipse(x, y, w, h, outline) {
 		CTX.beginPath();
 		CTX.moveTo(x, y + h * 0.5);
 		CTX.quadraticCurveTo(x, y, x + w * 0.5, y);
@@ -265,8 +277,8 @@ class Draw {
 		CTX.quadraticCurveTo(x, y + h, x, y + h * 0.5);
 		CTX.closePath();
 		this.draw(outline);
-	}
-	static roundrect(x, y, w, h, r, outline) {
+	},
+	roundrect(x, y, w, h, r, outline) {
 		r = Math.clamp(r, 0, Math.min(w * 0.5, h * 0.5)) || 0;
 		CTX.beginPath();
 		CTX.moveTo(x, y + r);
@@ -279,82 +291,171 @@ class Draw {
 		CTX.quadraticCurveTo(x, y + h, x, y + h - r);
 		CTX.closePath();
 		this.draw(outline);
-	}
-	static poly = {
-		name: '',
-		vertices: []
-	}
-	static polyBegin(poly) {
-		this.poly.name = poly;
-		this.poly.vertices = [];
-	}
-	static vertex(x, y) {
-		this.poly.vertices.push({ x, y });
-	}
-	static polyEnd() {
+	},
+	polyType: '',
+	vertices: [],
+	polyBegin(polyType = Poly.fill) {
+		this.polyType = polyType;
+		this.vertices = [];
+	},
+	vertex(x, y) {
+		this.vertices.push({ x, y });
+	},
+	polyEnd(polyType) {
+		if (polyType) this.polyType = polyType;
 		const get = (p, s) => p.filter(x => x.includes(s))[0][1];
 		const split = (s) => s.replace(/\s/g, '').split(',').map(x => x.split(':'));
 		const getName = (s) => get(split(s), 'name');
-		const param = split(this.poly.name);
+		const param = split(this.polyType);
 		const name = get(param, 'name');
 		const outline = get(param, 'outline') === 'true';
+		const quantity = +get(param, 'quantity');
 		const closePath = get(param, 'closePath') === 'true';
-		switch (name) {
-			case getName(Poly.fill):
-			case getName(Poly.stroke):
-				let count = 0;
+		let count = 0;
+		CTX.beginPath();
+		for (const v of this.vertices) {
+			if (quantity === 1) {
+				this.draw(outline, Cap.round);
 				CTX.beginPath();
-				for (const v of this.poly.vertices) {
-					if (count === 0) CTX.moveTo(v.x, v.y);
-					else CTX.lineTo(v.x, v.y);
-					count++;
-				}
+				CTX.moveTo(v.x, v.y);
+				CTX.lineTo(v.x, v.y);
+			}
+			else if (count === 0 || (quantity > 1 && count % quantity === 0)) {
 				if (closePath) CTX.closePath();
 				this.draw(outline);
-				break;
+				CTX.beginPath();
+				CTX.moveTo(v.x, v.y);
+			}
+			else CTX.lineTo(v.x, v.y);
+			count++;
 		}
-	}
-	static setCap(cap) {
+		if (closePath) CTX.closePath();
+		this.draw(outline);
+	},
+	rectTransformed(x, y, w, h, d, outline) {
+		const r = Math.hypot(w * 0.5, h * 0.5);
+		const p = [
+			Math.lendir(r, d + 225),
+			Math.lendir(r, d + 315),
+			Math.lendir(r, d + 45),
+			Math.lendir(r, d + 135)
+		];
+		this.polyBegin(outline? Poly.stroke : Poly.fill);
+		this.vertex(x + p[0].x, y + p[0].y);
+		this.vertex(x + p[1].x, y + p[1].y);
+		this.vertex(x + p[2].x, y + p[2].y);
+		this.vertex(x + p[3].x, y + p[3].y);
+		this.polyEnd();
+	},
+	starTransformed(x, y, r, d, outline) {
+		const lp = [
+			Math.lendir(r, d + 270),
+			Math.lendir(r, d + 342),
+			Math.lendir(r, d + 54),
+			Math.lendir(r, d + 126),
+			Math.lendir(r, d + 198)
+		];
+		const sr = r * 0.5;
+		const sp = [
+			Math.lendir(sr, d + 306),
+			Math.lendir(sr, d + 18),
+			Math.lendir(sr, d + 90),
+			Math.lendir(sr, d + 162),
+			Math.lendir(sr, d + 234)
+		];
+		this.polyBegin(outline? Poly.stroke : Poly.fill);
+		this.vertex(x + lp[0].x, y + lp[0].y);
+		this.vertex(x + sp[0].x, y + sp[0].y);
+		this.vertex(x + lp[1].x, y + lp[1].y);
+		this.vertex(x + sp[1].x, y + sp[1].y);
+		this.vertex(x + lp[2].x, y + lp[2].y);
+		this.vertex(x + sp[2].x, y + sp[2].y);
+		this.vertex(x + lp[3].x, y + lp[3].y);
+		this.vertex(x + sp[3].x, y + sp[3].y);
+		this.vertex(x + lp[4].x, y + lp[4].y);
+		this.vertex(x + sp[4].x, y + sp[4].y);
+		this.polyEnd();
+	},
+	star(x, y, r, outline) {
+		this.starTransformed(x, y, r, 0, outline);
+	},
+	setCap(cap) {
 		CTX.lineCap = cap;
-	}
-	static resetCap() {
+	},
+	resetCap() {
 		CTX.lineCap = Cap.butt;
-	}
-	static clearRect(x, y, w, h) {
+	},
+	setLineWidth(n) {
+		CTX.lineWidth = n;
+	},
+	resetLineWidth() {
+		CTX.lineWidth = 1;
+	},
+	beginPath() {
+		CTX.beginPath();
+	},
+	closePath() {
+		CTX.closePath();
+	},
+	moveTo(x, y) {
+		CTX.moveTo(x, y);
+	},
+	lineTo(x, y) {
+		CTX.lineTo(x, y);
+	},
+	curveTo(cpx, cpy, x, y) {
+		CTX.quadraticCurveTo(cpx, cpy, x, y);
+	},
+	bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y) {
+		CTX.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+	},
+	fill() {
+		CTX.fill();
+	},
+	stroke() {
+		CTX.stroke();
+	},
+	clearRect(x, y, w, h) {
 		CTX.clearRect(x, y, w, h);
 	}
-}
+};
 
+let ID = 0;
 class BranthObject {
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
+		this.id = ID++;
+		this.active = true;
+		this.visible = true;
+		this.alarm = [-1, -1, -1, -1, -1, -1];
 	}
-	static ID = 0;
-	id = BranthObject.ID++;
-	active = true;
-	visible = true;
+	start() {}
 	update() {}
 	render() {}
 	renderUI() {}
-	alarm = [];
-	alarmFunction = [
-		this.alarm0,
-		this.alarm1,
-		this.alarm2,
-		this.alarm3,
-		this.alarm4,
-		this.alarm5
-	];
+	alarm0() {}
+	alarm1() {}
+	alarm2() {}
+	alarm3() {}
+	alarm4() {}
+	alarm5() {}
 	alarmUpdate() {
 		if (this.alarm) {
-			for (const i in this.alarm) {
+			for (let i = 0; i < this.alarm.length; i++) {
 				if (this.alarm[i] !== null) {
 					if (this.alarm[i] > 0) {
 						this.alarm[i] = Math.max(0, this.alarm[i] - Time.deltaTime);
 					}
 					else if (this.alarm[i] != -1) {
-						if (this.alarmFunction[i]) this.alarmFunction[i]();
+						switch (i) {
+							case 0: this.alarm0(); break;
+							case 1: this.alarm1(); break;
+							case 2: this.alarm2(); break;
+							case 3: this.alarm3(); break;
+							case 4: this.alarm4(); break;
+							case 5: this.alarm5(); break;
+						}
 						if (this.alarm[i] <= 0) this.alarm[i] = -1;
 					}
 				}
@@ -363,25 +464,21 @@ class BranthObject {
 	}
 }
 
-class OBJ {
-	static list = [];
-	static classes = [];
-	static add(cls) {
+const OBJ = {
+	list: [],
+	classes: [],
+	add(cls) {
 		this.list.push([]);
 		this.classes.push(cls);
-	}
-	static create(cls, x, y) {
+	},
+	create(cls, x, y) {
 		if (this.classes.includes(cls)) {
-			const i = new cls(x, y);
-			this.list[this.classes.indexOf(cls)].push(i);
+			const i = new cls(x, y); this.list[this.classes.indexOf(cls)].push(i); i.start();
 			return i;
 		}
-		else {
-			console.log(`Class not found: ${cls}`);
-			return null;
-		}
-	}
-	static update() {
+		console.log(`Class not found: ${cls.name}\n- Try add OBJ.add(${cls.name}); in your code.`);
+	},
+	update() {
 		for (const o of this.list) {
 			for (const i of o) {
 				if (i.active) {
@@ -393,7 +490,7 @@ class OBJ {
 			}
 		}
 	}
-}
+};
 
 const UI = {
 	update() {
@@ -405,7 +502,7 @@ const UI = {
 			}
 		}
 	}
-}
+};
 
 class BranthRoom {
 	constructor(name, w, h) {
@@ -416,31 +513,31 @@ class BranthRoom {
 	start() {}
 }
 
-class Room {
-	_name = '';
-	static list = [];
-	static get names() {
+const Room = {
+	_name: '',
+	list: [],
+	get names() {
 		return this.list.map(x => x.name);
-	}
-	static get id() {
+	},
+	get id() {
 		return this.names.indexOf(this._name);
-	}
-	static get name() {
+	},
+	get name() {
 		return this._name;
-	}
-	static get current() {
+	},
+	get current() {
 		return this.list[this.id] || new BranthRoom(this._name, CANVAS.width, CANVAS.height);
-	}
-	static get w() {
+	},
+	get w() {
 		return this.current.w;
-	}
-	static get h() {
+	},
+	get h() {
 		return this.current.h;
-	}
-	static add(room) {
+	},
+	add(room) {
 		this.list.push(room);
-	}
-	static start(name) {
+	},
+	start(name) {
 		if (this.names.includes(name)) {
 			this._name = name;
 			CANVAS.width = this.w;
@@ -451,7 +548,7 @@ class Room {
 			console.log(`Room not found: ${name}`);
 		}
 	}
-}
+};
 
 const RAF = window.requestAnimationFrame
 	|| window.msRequestAnimationFrame

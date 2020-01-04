da4ad3ff20f97a683ba60ea7a52ca720df179cb3
branthnl <branthnl1@gmail.com>
2020-01-03 20:37:22 -0700

    new branth room system

--- 013-froggy/app.js
+++ 013-froggy/app.js
@@ -1,73 +1,3 @@
-Number.prototype.mid = function() { return this.valueOf() * 0.5; }
-Math.clamp = (a, b, c) => Math.min(c, Math.max(b, a));
-Math.range = (min, max) => min + Math.random() * (max - min);
-Math.degtorad = (d = 1) => d * Math.PI / 180;
-Math.radtodeg = (d = 1) => d * 180 / Math.PI;
-Math.lendirx = (l, d) => l * Math.cos(Math.degtorad(d));
-Math.lendiry = (l, d) => l * Math.sin(Math.degtorad(d));
-Math.lendir = (l, d) => ({ x: Math.lendirx(l, d), y: Math.lendiry(l, d) });
-Math.randneg = (t = 0.5) => Math.range(0, 1) > t? -1 : 1;
-
-const FRAME_RATE = 1000 / 60;
-
-const Time = {
-	time: 0,
-	lastTime: 0,
-	deltaTime: 0,
-	fixedDeltaTime: FRAME_RATE,
-	update(t) {
-		this.lastTime = this.time || 0;
-		this.time = t || 0;
-		this.deltaTime = this.time - this.lastTime || this.fixedDeltaTime;
-	}
-};
-
-class BranthRoom {
-	constructor(name, w, h) {
-		this.name = name;
-		this.w = w;
-		this.h = h;
-	}
-}
-
-const Room = {
-	r: [],
-	keys: [],
-	add(room) {
-		this.r.push(room);
-		this.keys.push(room.name);
-	}
-};
-
-class BranthObject {
-	static ID = 0;
-	static getID() { return BranthObject.ID++; }
-	id = BranthObject.getID();
-	depth = 0;
-	update() {}
-	render() {}
-	renderUI() {}
-	alarm = [];
-	alarmFunction = [this.alarm0, this.alarm1, this.alarm2, this.alarm3, this.alarm4, this.alarm5];
-	alarmUpdate() {
-		if (this.alarm) {
-			for (const i in this.alarm) {
-				if (this.alarm[i] !== null) {
-					if (this.alarm[i] > 0) {
-						this.alarm[i] = Math.max(0, this.alarm[i] - Time.deltaTime);
-					}
-					else if (this.alarm[i] != -1) {
-						if (this.alarmFunction[i]) {
-							this.alarmFunction[i]();
-						}
-						if (this.alarm[i] <= 0) this.alarm[i] = -1;
-					}
-				}
-			}
-		}
-	}
-}
-
 class Frog extends BranthObject {
 	depth = 0;
 }
@@ -76,55 +6,21 @@ class Lotus extends BranthObject {
 	depth = 1;
 }
 
-const OBJ = {
-	o: [],
-	keys: [],
-	getIndex(k) { return this.keys.indexOf(k) },
-	add(instance) {
-		const k = instance.constructor;
-		if (!this.keys.includes(k)) {
-			this.o.push([]);
-			this.keys.push(k);
-		}
-		const i = this.getIndex(k);
-		this.o[i].push(instance);
-	},
-	update() {
-		for (object of this.o) {
-			for (instance of object) {
-				object.update();
-			}
-		}
-	}
-};
+const Menu = new BranthRoom('Menu', 640, 640);
+const Game = new BranthRoom('Game', 240, 360);
+Room.add(Menu);
+Room.add(Game);
 
-const UI = {
-	update() {
-		for (object of OBJ.o) {
-			for (instance of object) {
-				object.renderUI();
-			}
-		}
-	}
+const frog1 = new Frog();
+
+OBJ.add(frog1);
+
+Menu.start = () => {
 }
 
-const RAF = window.requestAnimationFrame
-	|| window.msRequestAnimationFrame
-	|| window.mozRequestAnimationFrame
-	|| window.webkitRequestAnimationFrame
-	|| function(f) { return setTimeout(f, FRAME_RATE) }
-const BRANTH = {
-	start: function() {
-		this.update();
-	},
-	update: function(t) {
-		Time.update(t);
-		OBJ.update();
-		UI.update();
-		RAF(BRANTH.update);
-	}
-};
-
-window.onload = () => {
-	BRANTH.start();
+Game.start = () => {
+	console.log('game');
 }
+
+BRANTH.start();
+Room.start('Menu');
---
+++ 013-froggy/branth.js
@@ -0,0 +1,166 @@
+Number.prototype.mid = function() { return this.valueOf() * 0.5; }
+Math.clamp = (a, b, c) => Math.min(c, Math.max(b, a));
+Math.range = (min, max) => min + Math.random() * (max - min);
+Math.degtorad = (d = 1) => d * Math.PI / 180;
+Math.radtodeg = (d = 1) => d * 180 / Math.PI;
+Math.lendirx = (l, d) => l * Math.cos(Math.degtorad(d));
+Math.lendiry = (l, d) => l * Math.sin(Math.degtorad(d));
+Math.lendir = (l, d) => ({ x: Math.lendirx(l, d), y: Math.lendiry(l, d) });
+Math.randneg = (t = 0.5) => Math.range(0, 1) > t? -1 : 1;
+
+const PARENT = document.body;
+const CANVAS = document.createElement('canvas');
+const FRAME_RATE = 1000 / 60;
+
+const Time = {
+	time: 0,
+	lastTime: 0,
+	deltaTime: 0,
+	fixedDeltaTime: FRAME_RATE,
+	update(t) {
+		this.lastTime = this.time || 0;
+		this.time = t || 0;
+		this.deltaTime = this.time - this.lastTime || this.fixedDeltaTime;
+	}
+};
+
+class BranthObject {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	static ID = 0;
+	static getID() { return BranthObject.ID++; }
+	id = BranthObject.getID();
+	depth = 0;
+	active = true;
+	visible = true;
+	update() {}
+	render() {}
+	renderUI() {}
+	alarm = [];
+	alarmFunction = [this.alarm0, this.alarm1, this.alarm2, this.alarm3, this.alarm4, this.alarm5];
+	alarmUpdate() {
+		if (this.alarm) {
+			for (const i in this.alarm) {
+				if (this.alarm[i] !== null) {
+					if (this.alarm[i] > 0) {
+						this.alarm[i] = Math.max(0, this.alarm[i] - Time.deltaTime);
+					}
+					else if (this.alarm[i] != -1) {
+						if (this.alarmFunction[i]) {
+							this.alarmFunction[i]();
+						}
+						if (this.alarm[i] <= 0) this.alarm[i] = -1;
+					}
+				}
+			}
+		}
+	}
+}
+
+const OBJ = {
+	o: [],
+	classes: [],
+	add(i) {
+		const c = i.constructor;
+		if (!this.classes.includes(c)) {
+			this.o.push([]);
+			this.classes.push(c);
+		}
+		this.o[this.classes.indexOf(c)].push(i);
+	},
+	update() {
+		for (const o of this.o) {
+			for (const i of o) {
+				if (i.active) {
+					i.update();
+				}
+				if (i.visible) {
+					i.render();
+				}
+			}
+		}
+	},
+	clearAll() {
+		for (const i in this.o) {
+			this.o[i] = [];
+		}
+	}
+};
+
+const UI = {
+	update() {
+		for (const o of OBJ.o) {
+			for (const i of o) {
+				if (i.visible) {
+					i.renderUI();
+				}
+			}
+		}
+	}
+}
+
+class BranthRoom {
+	constructor(name, w, h) {
+		this.name = name;
+		this.w = w;
+		this.h = h;
+	}
+	start() {}
+}
+
+class Room {
+	_name = '';
+	static list = [];
+	static get names() {
+		return this.list.map(x => x.name);
+	}
+	static get id() {
+		return this.names.indexOf(this._name);
+	}
+	static get name() {
+		return this._name;
+	}
+	static get current() {
+		return this.list[this.id];
+	}
+	static get w() {
+		return this.current.w;
+	}
+	static get h() {
+		return this.current.h;
+	}
+	static add(room) {
+		this.list.push(room);
+	}
+	static start(name) {
+		if (this.names.includes(name)) {
+			this._name = name;
+			CANVAS.width = this.w;
+			CANVAS.height = this.h;
+			this.current.start();
+		}
+		else {
+			console.log(`Room not found: ${name}`);
+		}
+	}
+}
+
+const RAF = window.requestAnimationFrame
+	|| window.msRequestAnimationFrame
+	|| window.mozRequestAnimationFrame
+	|| window.webkitRequestAnimationFrame
+	|| function(f) { return setTimeout(f, FRAME_RATE) }
+const BRANTH = {
+	start: function() {
+		PARENT.appendChild(CANVAS);
+		this.update();
+	},
+	update: function(t) {
+		Time.update(t);
+		OBJ.update();
+		UI.update();
+		RAF(BRANTH.update);
+	}
+};

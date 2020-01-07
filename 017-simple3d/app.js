7d1a172776e26e32cb781bf5711bd31e10251b83
branthnl <branthnl1@gmail.com>
2020-01-07 08:39:30 -0700

    add clipping

--- 017-simple3d/app.js
+++ 017-simple3d/app.js
@@ -30,7 +30,7 @@ class mat4x4 {
 const Game = new BranthRoom('Game', 640, 640);
 Room.add(Game);
 
-let meshCube, matProj;
+let meshCube, matProj, CAMERA;
 
 const multiplyMatrix = (i, m) => {
 	let o = new vec3d([0, 0, 0]);
@@ -82,6 +82,8 @@ Game.start = () => {
 	const ASPECT_RATIO = Room.h / Room.w;
 	const FOV_RAD = 1 / Math.tan(Math.degtorad(FOV * 0.5));
 
+	CAMERA = new vec3d([0, 0, 0]);
+
 	matProj = new mat4x4();
 	matProj.m[0][0] = ASPECT_RATIO * FOV_RAD;
 	matProj.m[1][1] = FOV_RAD;
@@ -114,6 +116,7 @@ Game.render = () => {
 
 	for (const tri of meshCube.tris) {
 
+		// Rotate in Z axis
 		let o0 = multiplyMatrix(tri.p[0], matRotZ);
 		let o1 = multiplyMatrix(tri.p[1], matRotZ);
 		let o2 = multiplyMatrix(tri.p[2], matRotZ);
@@ -124,6 +127,7 @@ Game.render = () => {
 			[o2.x, o2.y, o2.z]
 		]);
 
+		// Rotate in X axis
 		o0 = multiplyMatrix(triRotatedZ.p[0], matRotX);
 		o1 = multiplyMatrix(triRotatedZ.p[1], matRotX);
 		o2 = multiplyMatrix(triRotatedZ.p[2], matRotX);
@@ -134,39 +138,78 @@ Game.render = () => {
 			[o2.x, o2.y, o2.z]
 		]);
 
+
+		// Offset into the scene
 		let triTranslated = triRotatedZX;
 		triTranslated.p[0].z = triRotatedZX.p[0].z + 3;
 		triTranslated.p[1].z = triRotatedZX.p[1].z + 3;
 		triTranslated.p[2].z = triRotatedZX.p[2].z + 3;
 
-		o0 = multiplyMatrix(triTranslated.p[0], matProj);
-		o1 = multiplyMatrix(triTranslated.p[1], matProj);
-		o2 = multiplyMatrix(triTranslated.p[2], matProj);
-
-		let triProjected = new triangle([
-			[o0.x, o0.y, o0.z],
-			[o1.x, o1.y, o1.z],
-			[o2.x, o2.y, o2.z]
+		const line1 = new vec3d([
+			triTranslated.p[1].x - triTranslated.p[0].x,
+			triTranslated.p[1].y - triTranslated.p[0].y,
+			triTranslated.p[1].z - triTranslated.p[0].z
 		]);
 
-		triProjected.p[0].x += 1; triProjected.p[0].y += 1;
-		triProjected.p[1].x += 1; triProjected.p[1].y += 1;
-		triProjected.p[2].x += 1; triProjected.p[2].y += 1;
+		const line2 = new vec3d([
+			triTranslated.p[2].x - triTranslated.p[0].x,
+			triTranslated.p[2].y - triTranslated.p[0].y,
+			triTranslated.p[2].z - triTranslated.p[0].z
+		]);
+
+		const normal = new vec3d([
+			line1.y * line2.z - line1.z * line2.y,
+			line1.z * line2.x - line1.x * line2.z,
+			line1.x * line2.y - line1.y * line2.x
+		]);
+
+		const l = Math.sqrt(normal.x ** 2 + normal.y ** 2 + normal.z ** 2);
+		normal.x /= l;
+		normal.y /= l;
+		normal.z /= l;
+
+		// Project triangles from 3D to 2D
+		// if (normal.z < 0) {
+		if (normal.x * (triTranslated.p[0].x - CAMERA.x) +
+			normal.y * (triTranslated.p[0].y - CAMERA.y) +
+			normal.z * (triTranslated.p[0].z - CAMERA.z) < 0) {
+			o0 = multiplyMatrix(triTranslated.p[0], matProj);
+			o1 = multiplyMatrix(triTranslated.p[1], matProj);
+			o2 = multiplyMatrix(triTranslated.p[2], matProj);
+
+			let triProjected = new triangle([
+				[o0.x, o0.y, o0.z],
+				[o1.x, o1.y, o1.z],
+				[o2.x, o2.y, o2.z]
+			]);
+
+			triProjected.p[0].x += 1; triProjected.p[0].y += 1;
+			triProjected.p[1].x += 1; triProjected.p[1].y += 1;
+			triProjected.p[2].x += 1; triProjected.p[2].y += 1;
 
 
-		triProjected.p[0].x *= 0.5 * Room.w;
-		triProjected.p[0].y *= 0.5 * Room.h;
-		triProjected.p[1].x *= 0.5 * Room.w;
-		triProjected.p[1].y *= 0.5 * Room.h;
-		triProjected.p[2].x *= 0.5 * Room.w;
-		triProjected.p[2].y *= 0.5 * Room.h;
+			triProjected.p[0].x *= 0.5 * Room.w;
+			triProjected.p[0].y *= 0.5 * Room.h;
+			triProjected.p[1].x *= 0.5 * Room.w;
+			triProjected.p[1].y *= 0.5 * Room.h;
+			triProjected.p[2].x *= 0.5 * Room.w;
+			triProjected.p[2].y *= 0.5 * Room.h;
 
-		Draw.setColor(C.black);
-		Draw.polyBegin(Poly.stroke);
-		Draw.vertex(triProjected.p[0].x, triProjected.p[0].y);
-		Draw.vertex(triProjected.p[1].x, triProjected.p[1].y);
-		Draw.vertex(triProjected.p[2].x, triProjected.p[2].y);
-		Draw.polyEnd();
+			Draw.setColor(C.darkGray);
+			Draw.polyBegin(Poly.fill);
+			Draw.vertex(triProjected.p[0].x, triProjected.p[0].y);
+			Draw.vertex(triProjected.p[1].x, triProjected.p[1].y);
+			Draw.vertex(triProjected.p[2].x, triProjected.p[2].y);
+			Draw.polyEnd();
+			Draw.setColor(C.black);
+			Draw.polyEnd(Poly.stroke);
+			Draw.setLineWidth(7);
+			Draw.polyEnd(Poly.pointList);
+			Draw.setLineWidth(5);
+			Draw.setColor(C.yellow);
+			Draw.polyEnd(Poly.pointList);
+			Draw.resetLineWidth();
+		}
 
 	}
 }

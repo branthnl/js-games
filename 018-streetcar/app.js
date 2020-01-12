68b5aa8fa8088d1905620db4d0825b0fbc15a226
branthnl <branthnl1@gmail.com>
2020-01-11 17:59:26 -0700

    Recreate simplify method

--- 018-streetcar/app.js
+++ 018-streetcar/app.js
@@ -1,3 +1,81 @@
+class Vector3 {
+	constructor(x = 0, y = 0, z = 0, w = 1) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+	}
+	normalize() {
+		const l = Vector3.len(this);
+		this.x /= l;
+		this.y /= l;
+		this.z /= l;
+	}
+	static add(v1, v2) {
+		return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+	}
+	static sub(v1, v2) {
+		return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+	}
+	static mul(v1, k) {
+		return new Vector3(v1.x * k, v1.y * k, v1.z * k);
+	}
+	static div(v1, k) {
+		return new Vector3(v1.x / k, v1.y / k, v1.z / k);
+	}
+	static dot(v1, v2) {
+		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	}
+	static len(v) {
+		return Math.sqrt(Vector3.dot(v, v));
+	}
+	static cross(v1, v2) {
+		return new Vector3(
+			v1.y * v2.z - v1.z * v2.y,
+			v1.z * v2.x - v1.x * v2.z,
+			v1.x * v2.y - v1.y * v2.x
+		);
+	}
+}
+
+class Triangle {
+	constructor(...points) {
+		this.p = [];
+		if (points.length >= 3) {
+			this.p.push(new Vector3(points[0].x, points[0].y, points[0].z));
+			this.p.push(new Vector3(points[1].x, points[1].y, points[1].z));
+			this.p.push(new Vector3(points[2].x, points[2].y, points[2].z));
+		}
+		else {
+			this.p = [new Vector3(), new Vector3(), new Vector3()];
+		}
+	}
+}
+
+class Mesh {
+	constructor() {
+		this.tris = [];
+	}
+	loadFromFile(fileString) {
+		this.tris = [];
+		const vtx = [];
+		const n = fileString.split(/\r\n|\n/);
+		for (let i = 0; i < n.length; i++) {
+			const m = n[i].split(' ');
+			if (m.length > 3) {
+				const id = m[0];
+				const x = +m[1];
+				const y = +m[2];
+				const z = +m[3];
+				switch (id) {
+					case 'v': vtx.push(new Vector3(x, y, z)); break;
+					case 'f': this.tris.push(new Triangle(vtx[x - 1], vtx[y - 1], vtx[z - 1])); break;
+				}
+			}
+		}
+	}
+}
+
 class Mat4x4 {
 	constructor() {
 		this.m = [
@@ -6,14 +84,6 @@ class Mat4x4 {
 			[0, 0, 0, 0],
 			[0, 0, 0, 0]
 		];
-	}
-	static makeIdentity() {
-		const m = new Mat4x4();
-		m.m[0][0] = 1;
-		m.m[1][1] = 1;
-		m.m[2][2] = 1;
-		m.m[3][3] = 1;
-		return m;
 	}
 	static multiplyVector(m, v) {
 		return new Vector3(
@@ -34,6 +104,17 @@ class Mat4x4 {
 					m1.m[r][3] * m2.m[3][c];
 			}
 		}
+		return m;
+	}
+	static makeProjection(fovDeg, aspectRatio, near, far) {
+		const fovRad = 1 / Math.tan(Math.degtorad(fovDeg * 0.5));
+		const m = new Mat4x4();
+		m.m[0][0] = aspectRatio * fovRad;
+		m.m[1][1] = fovRad;
+		m.m[2][2] = far / (far - near);
+		m.m[3][2] = (-far * near) / (far - near);
+		m.m[2][3] = 1;
+		m.m[3][3] = 0;
 		return m;
 	}
 	static makeRotationX(angleRad) {
@@ -66,109 +147,15 @@ class Mat4x4 {
 		m.m[3][3] = 1;
 		return m;
 	}
-	static makeTranslation(x, y, z) {
-		const m = new Mat4x4();
-		m.m[0][0] = 1;
-		m.m[1][1] = 1;
-		m.m[2][2] = 1;
-		m.m[3][3] = 1;
-		m.m[3][0] = x;
-		m.m[3][1] = y;
-		m.m[3][2] = z;
-		return m;
-	}
-	static makeProjection(fovDeg, aspectRatio, near, far) {
-		const fovRad = 1 / Math.tan(Math.degtorad(fovDeg * 0.5));
-		const m = new Mat4x4();
-		m.m[0][0] = aspectRatio * fovRad;
-		m.m[1][1] = fovRad;
-		m.m[2][2] = far / (far - near);
-		m.m[3][2] = (-far * near) / (far - near);
-		m.m[2][3] = 1;
-		m.m[3][3] = 0;
-		return m;
-	}
 }
 
-class Vector3 {
-	constructor(x = 0, y = 0, z = 0, w = 1) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.w = w;
+const multiplyMatrix = (m1, m2) => {
+	let v = Mat4x4.multiplyVector(m2, m1);
+	if (v.w != 0) {
+		v = Vector3.div(v, v.w);
 	}
-	static add(v1, v2) {
-		return new Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-	}
-	static sub(v1, v2) {
-		return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-	}
-	static mul(v1, k) {
-		return new Vector3(v1.x * k, v1.y * k, v1.z * k);
-	}
-	static div(v1, k) {
-		return new Vector3(v1.x / k, v1.y / k, v1.z / k);
-	}
-	static dot(v1, v2) {
-		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-	}
-	static cross(v1, v2) {
-		return new Vector3(
-			v1.y * v2.z - v1.z * v2.y,
-			v1.z * v2.x - v1.x * v2.z,
-			v1.x * v2.y - v1.y * v2.x
-		);
-	}
-	static len(v) {
-		return Math.sqrt(Vector3.dot(v, v));
-	}
-	static normalize(v) {
-		const l = Vector3.len(v);
-		return Vector3.div(v, l);
-	}
-}
-
-class Triangle {
-	constructor(...v) {
-		this.p = [];
-		if (v.length > 2) {
-			this.p.push(new Vector3(v[0].x, v[0].y, v[0].z));
-			this.p.push(new Vector3(v[1].x, v[1].y, v[1].z));
-			this.p.push(new Vector3(v[2].x, v[2].y, v[2].z));
-		}
-		else {
-			this.p = [
-				new Vector3(),
-				new Vector3(),
-				new Vector3()
-			];
-		}
-	}
-}
-
-class Mesh {
-	constructor() {
-		this.tris = [];
-	}
-	loadFromFile(fileString) {
-		this.tris = [];
-		const vtx = [];
-		const n = fileString.split(/\r\n|\n/);
-		for (let i = 0; i < n.length; i++) {
-			const m = n[i].split(' ');
-			if (m.length > 3) {
-				const id = m[0];
-				const x = +m[1];
-				const y = +m[2];
-				const z = +m[3];
-				switch (id) {
-					case 'v': vtx.push(new Vector3(x, y, z)); break;
-					case 'f': this.tris.push(new Triangle(vtx[x - 1], vtx[y - 1], vtx[z - 1])); break;
-				}
-			}
-		}
-	}
-}
+	return v;
+};
 
 const CAMERA = new Vector3();
 const MAT_PROJ = Mat4x4.makeProjection(90, Room.h / Room.w, 0.1, 1000);
@@ -189,44 +176,47 @@ class BranthObject3D extends BranthObject {
 		this.mesh = new Mesh();
 	}
 	get matRx() {
-		return Mat4x4.makeRotationX(this.rx);
+		return Mat4x4.makeRotationX(Math.degtorad(this.rx));
 	}
 	get matRy() {
-		return Mat4x4.makeRotationY(this.ry);
+		return Mat4x4.makeRotationY(Math.degtorad(this.ry));
 	}
 	get matRz() {
-		return Mat4x4.makeRotationZ(this.rz);
-	}
-	get matTrans() {
-		return Mat4x4.makeTranslation(this.x, this.y, this.z);
+		return Mat4x4.makeRotationZ(Math.degtorad(this.rz));
 	}
 	render() {
-		let matWorld;
-		matWorld = Mat4x4.multiplyMatrix(this.matRx, this.matRy);
-		matWorld = Mat4x4.multiplyMatrix(matWorld, this.matRz);
-		matWorld = Mat4x4.multiplyMatrix(matWorld, this.matTrans);
 		const trisToRaster = [];
+		let matWorld = Mat4x4.multiplyMatrix(Mat4x4.multiplyMatrix(this.matRx, this.matRy), this.matRz);
 		for (const tri of this.mesh.tris) {
 			const triTransformed = new Triangle(
-				new Vector3(Mat4x4.multiplyVector(matWorld, tri.p[0])),
-				new Vector3(Mat4x4.multiplyVector(matWorld, tri.p[1])),
-				new Vector3(Mat4x4.multiplyVector(matWorld, tri.p[2]))
+				multiplyMatrix(tri.p[0], matWorld),
+				multiplyMatrix(tri.p[1], matWorld),
+				multiplyMatrix(tri.p[2], matWorld)
 			);
+			triTransformed.p[0].x += this.x;
+			triTransformed.p[0].y += this.y;
+			triTransformed.p[0].z += this.z;
+			triTransformed.p[1].x += this.x;
+			triTransformed.p[1].y += this.y;
+			triTransformed.p[1].z += this.z;
+			triTransformed.p[2].x += this.x;
+			triTransformed.p[2].y += this.y;
+			triTransformed.p[2].z += this.z;
 			const line1 = Vector3.sub(triTransformed.p[1], triTransformed.p[0]);
 			const line2 = Vector3.sub(triTransformed.p[2], triTransformed.p[0]);
-			const normal = Vector3.normalize(Vector3.cross(line1, line2));
+			const normal = Vector3.cross(line1, line2);
+			normal.normalize();
 			const cameraRay = Vector3.sub(triTransformed.p[0], CAMERA);
 			if (Vector3.dot(normal, cameraRay) < 0) {
-				const light = Vector3.normalize(new Vector3(0, 0, -1));
-				const dp = Math.max(0.1, Vector3.dot(light, normal));
-				const triProjected = new Triangle(
-					Mat4x4.multiplyVector(MAT_PROJ, triTransformed.p[0]),
-					Mat4x4.multiplyVector(MAT_PROJ, triTransformed.p[1]),
-					Mat4x4.multiplyVector(MAT_PROJ, triTransformed.p[2])
-				);
-				triProjected.p.map(v => Vector3.div(v, v.w));
-				const offsetView = new Vector3(1, 1, 0);
-				triProjected.p.map(v => Vector3.add(v, offsetView));
+				const light = new Vector3(0, 0, -1); light.normalize();
+				const dp = Vector3.dot(normal, light);
+				const triProjected = new Triangle();
+				triProjected.p[0] = multiplyMatrix(triTransformed.p[0], MAT_PROJ);
+				triProjected.p[1] = multiplyMatrix(triTransformed.p[1], MAT_PROJ);
+				triProjected.p[2] = multiplyMatrix(triTransformed.p[2], MAT_PROJ);
+				triProjected.p[0].x += 1; triProjected.p[0].y += 1;
+				triProjected.p[1].x += 1; triProjected.p[1].y += 1;
+				triProjected.p[2].x += 1; triProjected.p[2].y += 1;
 				triProjected.p[0].x *= 0.5 * Room.w;
 				triProjected.p[0].y *= 0.5 * Room.h;
 				triProjected.p[1].x *= 0.5 * Room.w;
@@ -234,7 +224,6 @@ class BranthObject3D extends BranthObject {
 				triProjected.p[2].x *= 0.5 * Room.w;
 				triProjected.p[2].y *= 0.5 * Room.h;
 				triProjected.dp = dp;
-				if (Time.time < 1000) console.log(triProjected);
 				trisToRaster.push(triProjected);
 			}
 		}
@@ -244,7 +233,7 @@ class BranthObject3D extends BranthObject {
 			return z1 > z2? -1 : 1;
 		});
 		for (const tri of trisToRaster) {
-			const c = Math.abs(tri.dp) * 200;
+			const c = tri.dp * 200;
 			Draw.setColor(`rgb(${c}, ${c}, ${c})`);
 			Draw.polyBegin(Poly.fill);
 			Draw.vertex(tri.p[0].x, tri.p[0].y);
@@ -274,20 +263,20 @@ class Cube extends BranthObject3D {
 		this.acc = 0.05;
 		this.mesh.tris = [
 			// SOUTH
-			new Triangle(new Vector3(0, 0, 0), new Vector3(0, -1, 0), new Vector3(1, -1, 0)),
-			new Triangle(new Vector3(0, 0, 0), new Vector3(1, -1, 0), new Vector3(1, 0, 0)),
+			new Triangle(new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0)),
+			new Triangle(new Vector3(0, 0, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0)),
 			// EAST
-			new Triangle(new Vector3(1, 0, 0), new Vector3(1, -1, 0), new Vector3(1, -1, 1)),
-			new Triangle(new Vector3(1, 0, 0), new Vector3(1, -1, 1), new Vector3(1, 0, 1)),
+			new Triangle(new Vector3(1, 0, 0), new Vector3(1, 1, 0), new Vector3(1, 1, 1)),
+			new Triangle(new Vector3(1, 0, 0), new Vector3(1, 1, 1), new Vector3(1, 0, 1)),
 			// NORTH
-			new Triangle(new Vector3(1, 0, 1), new Vector3(1, -1, 1), new Vector3(0, -1, 1)),
-			new Triangle(new Vector3(1, 0, 1), new Vector3(0, -1, 1), new Vector3(0, 0, 1)),
+			new Triangle(new Vector3(1, 0, 1), new Vector3(1, 1, 1), new Vector3(0, 1, 1)),
+			new Triangle(new Vector3(1, 0, 1), new Vector3(0, 1, 1), new Vector3(0, 0, 1)),
 			// WEST
-			new Triangle(new Vector3(0, 0, 1), new Vector3(0, -1, 1), new Vector3(0, -1, 0)),
-			new Triangle(new Vector3(0, 0, 1), new Vector3(0, -1, 0), new Vector3(0, 0, 0)),
+			new Triangle(new Vector3(0, 0, 1), new Vector3(0, 1, 1), new Vector3(0, 1, 0)),
+			new Triangle(new Vector3(0, 0, 1), new Vector3(0, 1, 0), new Vector3(0, 0, 0)),
 			// TOP
-			new Triangle(new Vector3(0, -1, 0), new Vector3(0, -1, 1), new Vector3(1, -1, 1)),
-			new Triangle(new Vector3(0, -1, 0), new Vector3(1, -1, 1), new Vector3(1, -1, 0)),
+			new Triangle(new Vector3(0, 1, 0), new Vector3(0, 1, 1), new Vector3(1, 1, 1)),
+			new Triangle(new Vector3(0, 1, 0), new Vector3(1, 1, 1), new Vector3(1, 1, 0)),
 			// BOTTOM
 			new Triangle(new Vector3(1, 0, 1), new Vector3(0, 0, 1), new Vector3(0, 0, 0)),
 			new Triangle(new Vector3(1, 0, 1), new Vector3(0, 0, 0), new Vector3(1, 0, 0))
@@ -335,8 +324,8 @@ class Cube extends BranthObject3D {
 				this.yto = -2;
 			}
 		}
-		// this.rxto += Time.deltaTime * 0.05;
-		// this.rzto += Time.deltaTime * 0.1;
+		this.rxto += Time.deltaTime * 0.07;
+		this.rzto += Time.deltaTime * 0.058;
 		this.x = Math.lerp(this.x, this.xto, 0.2);
 		this.y = Math.lerp(this.y, this.yto, 0.2);
 		this.z = Math.lerp(this.z, this.zto, 0.2);
@@ -394,7 +383,7 @@ document.body.appendChild(file);
 
 BRANTH.start();
 Room.start('Menu');
-const scaler = 2;
+const scaler = 8;
 CANVAS.width *= scaler;
 CANVAS.height *= scaler;
 CTX.scale(scaler, scaler);

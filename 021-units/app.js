ecb74581b9538f85fe2c42cf1197248842a97ce7
branthnl <branthnl1@gmail.com>
2020-01-15 11:48:25 -0700

    Click to move

--- 021-units/app.js
+++ 021-units/app.js
@@ -1,65 +1,43 @@
-// const Game = new BranthRoom('Game', 1280, 720);
-// Room.add(Game);
-// BRANTH.start();
-// Room.start('Game');
+Draw.add('Cars', 'Cars.png', 3, 3, 291, 220);
 
-const input = {
-	calendar1: [['9:00', '10:30'], ['12:00', '13:00'], ['16:00', '18:00']],
-	bound1: ['9:00', '20:00'],
-	calendar2: [['10:00', '11:30'], ['12:30', '14:30'], ['14:30', '15:00'], ['16:00', '17:00']],
-	bound2: ['10:00', '18:30'],
-	time: 30
-};
+const Game = new BranthRoom('Game', 1280, 720);
+Room.add(Game);
 
-// Expected output: [['11:30', '12:00'], ['15:00', '15:30'], ['18:00', '18:30']]
-
-const intToTime = (int) => {
-	const i = int;
-	const h = Math.floor(i / 60);
-	const m = i - h * 60;
-	return h + ':' + (m < 10? '0' + m : m);
-};
-const timeToInt = (time) => {
-	const t = time.split(':');
-	const h = +t[0] * 60;
-	const m = +t[1];
-	return h + m;
-};
-const getTimeDifference = (timeA, timeB) => {
-	const a = timeToInt(timeA);
-	const b = timeToInt(timeB);
-	return b - a;
-};
-const getAvailableSchedule = (input) => {
-	const getFreeTimeBlock = (calendar, bound) => {
-		const ftb = [];
-		for (let i = -1; i < calendar.length; i++) {
-			let endA, firstB;
-			if (i === -1) endA = bound[0];
-			else endA = calendar[i][1];
-			if (i === calendar.length - 1) firstB = bound[1];
-			else firstB = calendar[i + 1][0];
-			if (getTimeDifference(endA, firstB) >= input.time) {
-				ftb.push([endA, firstB]);
-			}
-		}
-		return ftb;
-	};
-	const ftb1 = getFreeTimeBlock(input.calendar1, input.bound1);
-	const ftb2 = getFreeTimeBlock(input.calendar2, input.bound2);
-	const availSchedule = [];
-	for (let i = 0; i < ftb1.length; i++) {
-		for (let j = 0; j < ftb2.length; j++) {
-			const a = ftb1[i];
-			const b = ftb2[j];
-			const latestStart = Math.max(timeToInt(a[0]), timeToInt(b[0]));
-			const earliestEnd = Math.min(timeToInt(a[1]), timeToInt(b[1]));
-			if (earliestEnd - latestStart >= input.time) {
-				availSchedule.push([intToTime(latestStart), intToTime(latestStart + input.time)]);
-			}
-		}
+class Unit extends BranthGameObject {
+	constructor(x, y, row, column, attack, defend, healthPoint, attackRange, movementSpeed) {
+		super(x, y);
+		this.row = row;
+		this.column = column;
+		this.attack = attack;
+		this.defend = defend;
+		this.healthPoint = healthPoint;
+		this.attackRange = attackRange;
+		this.movementSpeed = movementSpeed;
+		this.target = new Vector(0, 0);
+		this.spriteName = 'Cars';
 	}
-	return availSchedule;
+	moveToTarget() {
+		this.x = Math.lerp(this.x, this.target.x, this.movementSpeed / 32);
+		this.y = Math.lerp(this.y, this.target.y, this.movementSpeed / 32);
+	}
+	update() {
+		this.moveToTarget();
+	}
+}
+
+OBJ.add(Unit);
+
+Game.start = () => {
+	const n = new Unit(0, 0, 0, 0, 10, 10, 100, 5, 8);
+	OBJ.push(Unit, n);
 };
 
-console.log(getAvailableSchedule(input));
+Game.update = () => {
+	if (Input.mouseDown(0)) {
+		const m = Input.screenToWorldPoint(Input.mousePosition);
+		OBJ.take(Unit)[0].target = new Vector(m.x, m.y);
+	}
+};
+
+BRANTH.start();
+Room.start('Game');

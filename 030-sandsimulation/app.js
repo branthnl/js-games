f121e21ba2bf61f882ea5c58d8bba41309d01a1f
branthnl <branthnl1@gmail.com>
2020-02-02 17:40:59 -0700

    Add new game: sand simulation
    Click anywhere to deploy sand

---
+++ 030-sandsimulation/app.js
@@ -0,0 +1,29 @@
+class Sand extends BranthObject {
+	update() {
+		if (this.y < Room.h - 10) {
+			this.y += 10;
+		}
+	}
+	render() {
+		Draw.setColor(C.yellow);
+		Draw.rect(this.x, this.y, 10, 10);
+	}
+}
+
+OBJ.add(Sand);
+
+const Game = new BranthRoom('Game', 1280, 720);
+Room.add(Game);
+
+Game.update = () => {
+	if (Input.mouseHold(0)) {
+		const m = Input.screenToWorldPoint(Input.mousePosition);
+		OBJ.create(Sand, Math.round(m.x / 10) * 10, Math.round(m.y / 10) * 10);
+		if (OBJ.take(Sand).length > Room.w * Room.h * 0.01) {
+			OBJ.take(Sand).splice(0, 1);
+		}
+	}
+};
+
+BRANTH.start();
+Room.start('Game');

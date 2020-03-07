252a6ae4e1edbd958bbe132e715abb2a5238fb0b
branthnl <branthnl1@gmail.com>
2020-03-07 02:25:08 -0700

    failed

--- 046-autotile47/src/app.js
+++ 046-autotile47/src/app.js
@@ -1,19 +1,17 @@
 const Grid = [];
 
-class Wall extends BranthGameObject {
+class Wall extends BranthObject {
 	constructor(c, r) {
 		super(0, 0);
 		this.c = c;
 		this.r = r;
-		this.spriteName = 'tiles';
-		this.imageIndex = this.getAutoTileIndex();
 		this.updatePosition();
 	}
 	updatePosition() {
 		this.x = this.c * 64;
 		this.y = this.r * 64;
 	}
-	getAutoTileIndex() {
+	getWallAround() {
 		let i = 0;
 		let wallUp = false;
 		let wallLeft = false;
@@ -23,186 +21,56 @@ class Wall extends BranthGameObject {
 		let wallUpRight = false;
 		let wallDownLeft = false;
 		let wallDownRight = false;
-		if (this.r > 0) {
-			if (Grid[this.c][this.r - 1] !== null) {
-				wallUp = true;
-			}
-		}
-		if (this.c > 0) {
-			if (Grid[this.c - 1][this.r] !== null) {
-				wallLeft = true;
-			}
-		}
-		if (this.r < Grid[0].length - 1) {
-			if (Grid[this.c][this.r + 1] !== null) {
-				wallDown = true;
-			}
-		}
-		if (this.c < Grid.length - 1) {
-			if (Grid[this.c + 1][this.r] !== null) {
-				wallRight = true;
-			}
-		}
-		if (this.c > 0 && this.r > 0) {
-			if (Grid[this.c - 1][this.r - 1] !== null) {
-				wallUpLeft = true;
-			}
-		}
-		if (this.c < Grid.length - 1 && this.r > 0) {
-			if (Grid[this.c + 1][this.r - 1] !== null) {
-				wallUpRight = true;
-			}
-		}
-		if (this.c > 0 && this.r < Grid[0].length - 1) {
-			if (Grid[this.c - 1][this.r + 1] !== null) {
-				wallDownLeft = true;
-			}
-		}
-		if (this.c < Grid.length - 1 && this.r < Grid[0].length - 1) {
-			if (Grid[this.c + 1][this.r + 1] !== null) {
-				wallDownRight = true;
-			}
-		}
-		i = 44;
-		if (wallUp) {
-			i = 0;
-			if (wallRight) {
-				i = 4;
-				if (wallDown) {
-					i = 12;
-					if (wallLeft) {
-						i = 28;
-						if (wallUpRight) {
-							i = 29;
-							if (wallDownRight) {
-								i = 33;
-								if (wallDownLeft) {
-									i = 39;
-									if (wallUpLeft) {
-										i = 43;
-									}
-								}
-								else if (wallUpLeft) {
-									i = 43;
-								}
-							}
-							else if (wallDownLeft) {
-								i = 37;
-								if (wallUpLeft) {
-									i = 36;
-								}
-							}
-							else if (wallUpLeft) {
-								i = 36;
-							}
-						}
-						else if (wallDownRight) {
-							i = 30;
-							if (wallDownLeft) {
-								i = 34;
-								if (wallUpLeft) {
-									i = 42;
-								}
-							}
-							else if (wallUpLeft) {
-								i = 38;
-							}
-						}
-						else if (wallDownLeft) {
-							i = 31;
-							if (wallUpLeft) {
-								i = 35;
-							}
-						}
-						else if (wallUpLeft) {
-							i = 32;
-						}
-					}
-					else if (wallUpRight) {
-						i = 16;
-						if (wallDownRight) {
-							i = 18;
-						}
-					}
-					else if (wallDownRight) {
-						i = 17;
-					}
+		if (this.r > 0) if (Grid[this.c][this.r - 1] !== null) wallUp = true;
+		if (this.c > 0) if (Grid[this.c - 1][this.r] !== null) wallLeft = true;
+		if (this.r < Grid[0].length - 1) if (Grid[this.c][this.r + 1] !== null) wallDown = true;
+		if (this.c < Grid.length - 1) if (Grid[this.c + 1][this.r] !== null) wallRight = true;
+		if (this.c > 0 && this.r > 0) if (Grid[this.c - 1][this.r - 1] !== null) wallUpLeft = true;
+		if (this.c < Grid.length - 1 && this.r > 0) if (Grid[this.c + 1][this.r - 1] !== null) wallUpRight = true;
+		if (this.c > 0 && this.r < Grid[0].length - 1) if (Grid[this.c - 1][this.r + 1] !== null) wallDownLeft = true;
+		if (this.c < Grid.length - 1 && this.r < Grid[0].length - 1) if (Grid[this.c + 1][this.r + 1] !== null) wallDownRight = true;
+		return {
+			N: wallUp,
+			NE: wallUpRight,
+			E: wallRight,
+			SE: wallDownRight,
+			S: wallDown,
+			SW: wallDownLeft,
+			W: wallLeft,
+			NW: wallUpLeft
+		};
+	}
+	render() {
+		const img = this.getImage('tiles');
+		if (img) {
+			const sw = img.width / img.strip;
+			const s = {
+				w: img.width / img.strip,
+				h: img.height,
+				get x() {
+					return index * this.w;
+				},
+				y: 0
+			};
+			const d = {
+				w: s.w * xscale,
+				h: s.h * yscale,
+				get x() {
+					return -this.w * (xscale < 0? 1 - img.origin.x : img.origin.x);
+				},
+				get y() {
+					return -this.h * (yscale < 0? 1 - img.origin.y : img.origin.y);
 				}
-				else if (wallLeft) {
-					i = 15;
-					if (wallUpRight) {
-						i = 25;
-						if (wallUpLeft) {
-							i = 27;
-						}
-						else if (wallUpLeft) {
-							i = 26;
-						}
-					}
-					else if (wallUpRight) {
-						i = 8;
-					}
-				}
-				else if (wallDown) {
-					i = 45;
-					if (wallLeft) {
-						i = 14;
-						if (wallDownLeft) {
-							i = 22;
-							if (wallUpLeft) {
-								i = 24;
-							}
-						}
-						else if (wallUpLeft) {
-							i = 23;
-						}
-					}
-				}
-				else if (wallLeft) {
-					i = 7;
-					if (wallUpLeft) {
-						i = 11;
-					}
-				}
-			}
-			else if (wallRight) {
-				i = 1;
-				if (wallDown) {
-					i = 5;
-					if (wallLeft) {
-						i = 13;
-						if (wallDownRight) {
-							i = 19;
-							if (wallDownLeft) {
-								i = 21;
-							}
-						}
-						else if (wallDownLeft) {
-							i = 20;
-						}
-					}
-					else if (wallDownRight) {
-						i = 9;
-					}
-				}
-				else if (wallLeft) {
-					i = 46;
-				}
-			}
+			};
+			CTX.save();
+			CTX.translate(x, y);
+			CTX.scale(Math.sign(xscale), Math.sign(yscale));
+			CTX.rotate(rot * Math.PI / 180);
+			CTX.globalAlpha = alpha;
+			CTX.drawImage(img, s.x, s.y, s.w, s.h, d.x, d.y, d.w, d.h);
+			CTX.globalAlpha = 1;
+			CTX.restore();
 		}
-		else if (wallDown) {
-			i = 2;
-			if (wallLeft) {
-				i = 6;
-				if (wallDownLeft) {
-					i = 10;
-				}
-			}
-		}
-		else if (wallLeft) {
-			i = 3;
-		}
-		return i;
 	}
 }
 
@@ -212,7 +80,7 @@ const Game = new BranthRoom('Game');
 Room.add(Game);
 
 Game.start = () => {
-	Draw.addStrip(Vector2.zero, 'tiles', 'src/img/tile_template_strip47.png', 47);
+	Draw.add(Vector2.zero, 'tiles', 'src/img/grass_tile.png');
 	for (let i = 0; i < Room.w / 64; i++) {
 		Grid.push([]);
 		for (let j = 0; j < Room.h / 64; j++) {

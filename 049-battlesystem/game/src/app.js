7dfcb3199740acb4c343707bdf181488fcaf2a33
branthnl <branthnl1@gmail.com>
2020-03-30 14:26:38 -0700

    Allow unit to die

--- 049-battlesystem/game/src/app.js
+++ 049-battlesystem/game/src/app.js
@@ -27,8 +27,6 @@ BS.Data = {
 		Death: "Death",
 		Idle: "Idle",
 		JumpAndFall: "JumpAndFall",
-		Roll: "Roll",
-		Run: "Run",
 		Shield: "Shield"
 	},
 	GetUnitDataByIconIndex(i) {
@@ -61,15 +59,16 @@ BS.Spot = [
 class Unit extends BranthBehaviour {
 	constructor(team, index, position, name, strength, iconIndex) {
 		super(position.x, position.y);
-		this.team = team; // 0=army1, 1=army2
+		this.team = team; // army1 or army2
 		this.index = index; // 0-4
 		this.name = name;
 		this.strength = strength;
+		this.maxStrength = strength;
 		this.iconIndex = iconIndex;
 		this.data = BS.Data.GetUnitDataByIconIndex(this.iconIndex);
 		this.state = BS.Data.UnitState.Idle;
 		this.imageIndex = Math.irange(20);
-		this.imageXScale = this.team > 0? -1 : 1;
+		this.imageXScale = this.team === "army1"? 1 : -1;
 		this.actionInterval = {
 			min: 500,
 			max: 1000
@@ -84,6 +83,34 @@ class Unit extends BranthBehaviour {
 		this.imageIndex = 0;
 		this.state = newState;
 	}
+	getAttackStrength() {
+		return BS.Battle.averageDamage * Math.clamp(this.strength / this.maxStrength, 0.1, 1) * Math.range(1, 2);
+	}
+	takeDamage(amount) {
+		this.strength -= amount;
+		if (this.strength <= 0) {
+			// If our team should be win but this unit's death will make it lose, prevent death
+			if (this.team === BS.inputJSON.winner && this.getAlly().length < OBJ.take(Unit).length * 0.5) {
+				this.strength = this.maxStrength;
+				return;
+			}
+			// If this is the last unit from a team, they lost
+			if (OBJ.take())
+			this.changeState(BS.Data.UnitState.Death);
+		}
+	}
+	isAlly(x) {
+		return x.team === this.team;
+	}
+	isEnemy(x) {
+		return x.team !== this.team;
+	}
+	getAlly() {
+		return OBJ.take(Unit).filter(x => this.isAlly(x))
+	}
+	getEnemy() {
+		return OBJ.take(Unit).filter(x => this.isEnemy(x));
+	}
 	render() {
 		const name = `${this.data.spriteName}${this.state}`;
 		const img = Draw.getStrip(name);
@@ -91,7 +118,18 @@ class Unit extends BranthBehaviour {
 			case BS.Data.UnitState.Attack:
 				if (this.imageIndex >= img.strip) {
 					this.changeState(BS.Data.UnitState.Idle);
+					// Take random enemy to hit
+					try {
+						Math.pick(this.getEnemy()).takeDamage(this.getAttackStrength());
+					}
+					catch {}
 					this.alarm[1] = Math.range(this.actionInterval.min, this.actionInterval.max);
+				}
+				break;
+			case BS.Data.UnitState.Death:
+				if (this.imageIndex >= img.strip) {
+					OBJ.destroy(this.id);
+					return;
 				}
 				break;
 			case BS.Data.UnitState.JumpAndFall:
@@ -109,7 +147,6 @@ class Unit extends BranthBehaviour {
 		}
 		this.iconDrawY = Math.range(this.iconDrawY, this.y - img.height + Math.sin(Time.time * 0.01 + this.id) * 4, 0.1);
 		Draw.strip(name, this.imageIndex, this.x, this.y, this.imageXScale);
-		Draw.strip("icons", this.iconIndex, this.x, this.iconDrawY);
 	}
 	alarm0() {
 		// Animate by increasing image index
@@ -117,7 +154,7 @@ class Unit extends BranthBehaviour {
 		this.alarm[0] = 50;
 	}
 	alarm1() {
-		if (BS.Battle.time <= 0) return;
+		if (this.strength <= 0 || BS.Battle.time <= 0) return;
 		// Simple action
 		// Choose between: attack, defend, or dodge
 		if (this.attackCount < 3) {
@@ -150,9 +187,18 @@ BS.Battle = {
 	over: false,
 	duration: 15000,
 	resultBGAlpha: 0,
+	averageDamage: 0,
+	CreateAverageDamage() {
+		let dmg = 0;
+		for (const i of OBJ.take(Unit).map(x => +x.maxStrength)) {
+			dmg += i / 10;
+		}
+		return dmg / OBJ.take(Unit).length;
+	},
 	start() {
 		this.time = this.duration;
 		this.over = false;
+		this.averageDamage = this.CreateAverageDamage();
 	},
 	update() {
 		if (!this.over) {
@@ -177,11 +223,9 @@ Room.add(Game);
 Boot.start = () => {
 	Draw.addStrip(new Vector2(0.5, 1), "icons", "src/img/icons_strip3.png", 3);
 	Draw.addStrip(new Vector2(0.5, 1), "KnightAttack", "src/img/KnightAttack_strip26.png", 26);
-	// Draw.addStrip(new Vector2(0.5, 1), "KnightDeath", "src/img/KnightDeath_strip.png", 15);
+	Draw.addStrip(new Vector2(0.5, 1), "KnightDeath", "src/img/KnightDeath_strip15.png", 15);
 	Draw.addStrip(new Vector2(0.5, 1), "KnightIdle", "src/img/KnightIdle_strip15.png", 15);
 	Draw.addStrip(new Vector2(0.5, 1), "KnightJumpAndFall", "src/img/KnightJumpAndFall_strip12.png", 12);
-	// Draw.addStrip(new Vector2(0.5, 1), "KnightRoll", "src/img/KnightRoll_strip.png", 15);
-	// Draw.addStrip(new Vector2(0.5, 1), "KnightRun", "src/img/KnightRun_strip.png", 8);
 	Draw.addStrip(new Vector2(0.5, 1), "KnightShield", "src/img/KnightShield_strip20.png", 20);
 	fileInput.onchange = (e) => {
 		const f = e.target.files[0];
@@ -252,9 +296,11 @@ Game.renderUI = () => {
 			Draw.setFont(Font.m);
 			Draw.setColor(C.black);
 			for (const i of OBJ.take(Unit)) {
-				const unit = n[`army${i.team + 1}`][`s${i.index + 1}`];
-				txt = `#${i.index + 1} ${i.name}\nstr: ${i.strength}/${unit.strength}\nicon: ${i.iconIndex}`;
+				txt = `#${i.index + 1} ${i.name}\nstr: ${Math.ceil(i.strength)}/${i.maxStrength}\nicon: ${i.iconIndex}`;
 				Draw.text(i.x, i.y + 10, txt);
+			}
+			for (const i of OBJ.take(Unit)) {
+				Draw.strip("icons", i.iconIndex, i.x, i.iconDrawY);
 			}
 		}
 		Draw.setFont(Font.xl);
@@ -266,10 +312,10 @@ Game.renderUI = () => {
 			OBJ.clearAll();
 			let spotCount = 0;
 			for (let h = 0; h < 2; h++) {
-				let txt = "";
+				const team = `army${h + 1}`;
 				for (let i = 0; i < 5; i++) {
-					const unit = n[`army${h + 1}`][`s${i + 1}`];
-					OBJ.create(Unit, h, i, BS.Spot[spotCount++], unit.name, unit.strength, unit.iconIndex);
+					const unit = n[team][`s${i + 1}`];
+					OBJ.create(Unit, team, i, BS.Spot[spotCount++], unit.name, unit.strength, unit.iconIndex);
 				}
 			}
 			BS.Battle.start();
